8e18d90f46bfd113ff920034886af511240789c0
heejae <gmlwochl123@gmail.com>
2023-07-03 09:09:44 +0900

    다중상속

--- starCraft.py
+++ starCraft.py
@@ -1,16 +1,23 @@
+# 일반유닛
 class Unit:
-    def __init__(self, name, hp, damage):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
-        self.damage = damage
-        print("{0} 유닛이 생성되었습니다.".format(self.name))
-        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+        self.speed = speed
+        
+    def move(self, location):
+        print('{0} : {1} 방향으로 이동합니다. [속도 {2}]'.format(self.name, location, self.speed))
+        
+        # self.damage = damage
+        # print("{0} 유닛이 생성되었습니다.".format(self.name))
+        # print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
-
-class AttackUnit :
+# 공격유닛
+class AttackUnit (Unit) : # Unit class 상속
     def __init__(self, name, hp, damage):
-        self.name = name
-        self.hp = hp
+        # self.name = name
+        # self.hp = hp
+        Unit.__init__(self, name, hp) # 상속받은 class 사용
         self.damage = damage
         
     def attack(self, location) :
@@ -23,8 +30,17 @@ class AttackUnit :
         if self.hp <= 0 :
             print("{0} : 파괴되었습니다.".format(self.name))
 
-firebat1 = AttackUnit('파이어뱃', 50, 16)
-firebat1.attack("5시")
-firebat1.damaged(25)
-firebat1.damaged(25)
+# 날 수 있는 기능 class
+class Flyable :
+    def __init__(self, flying_speed) :
+        self.flying_speed = flying_speed
+        
+    def fly(self, name, location) :
+        print('{0} : {1} 방향으로 날아갑니다. [속도 : {2}]'.format(name, location, self.flying_speed))
+        
+# 공중 공격 유닛 (공격유닛 + 날 수 있는 기능) - 다중상속
+class FlyableAttackUnit (AttackUnit, Flyable) :
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
 
